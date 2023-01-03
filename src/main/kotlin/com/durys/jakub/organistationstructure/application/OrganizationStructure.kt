be9d76f5fac34e4da1a49ac73605c2d0a9fea77d
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-03 15:50:07 +0100

    init of organizationStructure application service

---
+++ src/main/kotlin/com/durys/jakub/organistationstructure/application/OrganizationStructure.kt
@@ -0,0 +1,10 @@
+package com.durys.jakub.organistationstructure.application
+
+import com.durys.jakub.organistationstructure.domain.StructureEntryRepository
+import org.springframework.stereotype.Component
+
+@Component
+class OrganizationStructure(private val structureEntryRepository: StructureEntryRepository) {
+
+
+}
