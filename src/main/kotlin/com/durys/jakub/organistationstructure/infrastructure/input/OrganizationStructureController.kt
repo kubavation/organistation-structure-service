1bc2e05d6142825d1612aa6b57e100e630caf5bc
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-03 19:36:50 +0100

    enpoint for creating structure entry

--- src/main/kotlin/com/durys/jakub/organistationstructure/infrastructure/input/OrganizationStructureController.kt
+++ src/main/kotlin/com/durys/jakub/organistationstructure/infrastructure/input/OrganizationStructureController.kt
@@ -1,10 +1,22 @@
 package com.durys.jakub.organistationstructure.infrastructure.input
 
 import com.durys.jakub.organistationstructure.application.OrganizationStructure
+import com.durys.jakub.organistationstructure.infrastructure.input.dto.StructureEntryDTO
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/")
-class OrganisationStructureController(private val organisationStructure: OrganizationStructure) {
+@RequestMapping("/api")
+class OrganizationStructureController(private val organisationStructure: OrganizationStructure) {
+
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun create(@RequestBody structure: StructureEntryDTO) {
+        organisationStructure.addStructure(structure.parentId, structure.name, structure.shortcut);
+    }
 }
