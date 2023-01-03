39813d487d6360a52c8686f1e46e796fefb7895b
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-03 18:53:30 +0100

    refactor

--- src/main/kotlin/com/durys/jakub/organistationstructure/application/OrganizationStructure.kt
+++ src/main/kotlin/com/durys/jakub/organistationstructure/application/OrganizationStructure.kt
@@ -1,6 +1,7 @@
 package com.durys.jakub.organistationstructure.application
 
 import com.durys.jakub.organistationstructure.domain.StructureEntry
+import com.durys.jakub.organistationstructure.domain.StructureEntryNotFoundException
 import com.durys.jakub.organistationstructure.domain.StructureEntryRepository
 import org.springframework.stereotype.Component
 import java.lang.RuntimeException
@@ -12,19 +13,18 @@ class OrganizationStructure(private val structureEntryRepository: StructureEntry
 
 
     fun addStructure(parentId: String?, name: String, shortcut: String) {
+
         if (parentId != null) {
 
-            val parent = structureEntryRepository.load(parentId)
-
-            if (parent == null) {
-                throw RuntimeException()
-            }
+            val parent = structureEntryRepository.load(parentId) ?: throw StructureEntryNotFoundException(parentId)
 
             parent.addDependant(StructureEntry(UUID.randomUUID().toString(), name, shortcut))
             structureEntryRepository.save(parent)
-        }
 
-        structureEntryRepository.save(StructureEntry(UUID.randomUUID().toString(), name, shortcut))
+        } else {
+            structureEntryRepository.save(StructureEntry(UUID.randomUUID().toString(), name, shortcut))
+        }
     }
+
 
 }
