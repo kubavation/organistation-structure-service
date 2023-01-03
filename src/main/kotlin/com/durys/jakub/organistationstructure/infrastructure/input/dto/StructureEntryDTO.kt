95012d769552c2e532a6a59b1e6816d86d60f740
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-03 19:38:35 +0100

    fix

--- src/main/kotlin/com/durys/jakub/organistationstructure/infrastructure/input/dto/StructureEntryDTO.kt
+++ src/main/kotlin/com/durys/jakub/organistationstructure/infrastructure/input/dto/StructureEntryDTO.kt
@@ -1,6 +1,6 @@
 package com.durys.jakub.organistationstructure.infrastructure.input.dto
 
-class StructureEntryDTO(val parentId: String?, val name: String?, val shortcut: String?) {
+class StructureEntryDTO(val parentId: String?, val name: String, val shortcut: String) {
 
-    constructor(): this(null, null, null)
+    constructor(): this(null, "","")
 }
