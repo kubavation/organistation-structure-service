98a0c0ed9d446c6e4263330dbd6e083df2f7729f
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-03 18:26:21 +0100

    change in structure entry -> mutable list

--- src/main/kotlin/com/durys/jakub/organistationstructure/domain/StructureEntry.kt
+++ src/main/kotlin/com/durys/jakub/organistationstructure/domain/StructureEntry.kt
@@ -3,7 +3,6 @@ package com.durys.jakub.organistationstructure.domain
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.data.mongodb.core.mapping.Field
-import java.util.*
 
 @Document("structure_entry")
 class StructureEntry (
@@ -12,7 +11,7 @@ class StructureEntry (
     val shortcut: String) {
 
     @Field("entries")
-    val entries: List<StructureEntry> = Collections.emptyList()
+    val entries: MutableList<StructureEntry> = mutableListOf()
     val status: String = ""
     val path: String = ""
 
