59374a67d0566383d805dc04f9230ef1efe46501
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-03 21:42:48 +0100

    cleaning

--- src/main/kotlin/com/durys/jakub/organistationstructure/infrastructure/output/RabbitmqEventPublisher.kt
+++ src/main/kotlin/com/durys/jakub/organistationstructure/infrastructure/output/RabbitmqEventPublisher.kt
@@ -1,9 +1,8 @@
 package com.durys.jakub.organistationstructure.infrastructure.output
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate
-import java.util.Objects
 
-class RabbitmqEventPublisher(val template: RabbitTemplate) {
+class RabbitmqEventPublisher(private val template: RabbitTemplate) {
 
     fun publish(event: String) { //todo
         template.convertAndSend(event)
