663a5610c50341ece2488bcba7aba911d84b3b73
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-03 18:17:08 +0100

    tests with parent

--- src/test/kotlin/com/durys/jakub/organistationstructure/application/OrganisationStructureTest.kt
+++ src/test/kotlin/com/durys/jakub/organistationstructure/application/OrganisationStructureTest.kt
@@ -3,6 +3,7 @@ package com.durys.jakub.organistationstructure.application
 import com.durys.jakub.organistationstructure.domain.StructureEntry
 import com.durys.jakub.organistationstructure.domain.StructureEntryRepository
 import com.durys.jakub.organistationstructure.infrastructure.output.MongoStructureEntryRepository
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
 import org.mockito.kotlin.any
@@ -25,6 +26,22 @@ class OrganisationStructureTest {
         organisationStructure.addStructure(parentId, name, shortcut)
 
         Mockito.verify(structureEntryRepository, Mockito.times(1)).save(any());
+    }
+
+    @Test
+    fun addStructureEntryWithParent_shouldSuccessfullyCreateEntry() {
+        val parentId = UUID.randomUUID().toString();
+        val name = "General department"
+        val shortcut = "GD"
+        val parent: StructureEntry = StructureEntry(parentId, "Department", "D1");
+
+
+        Mockito.`when`(structureEntryRepository.load(parentId)).thenReturn(parent)
+        organisationStructure.addStructure(parentId, name, shortcut)
+
+        Mockito.verify(structureEntryRepository, Mockito.times(1)).save(any());
+        Assertions.assertEquals(1, parent.entries.size)
+        Assertions.assertEquals(name, parent.entries[0].name)
     }
 
 }
