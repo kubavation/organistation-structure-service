b4bd97561310476bdfe6adc322627a8dc133b012
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-03 21:42:16 +0100

    config of event publisher

--- src/main/kotlin/com/durys/jakub/organistationstructure/infrastructure/OrganisationStructureConfiguration.kt
+++ src/main/kotlin/com/durys/jakub/organistationstructure/infrastructure/OrganisationStructureConfiguration.kt
@@ -2,6 +2,8 @@ package com.durys.jakub.organistationstructure.infrastructure
 
 import com.durys.jakub.organistationstructure.domain.StructureEntryRepository
 import com.durys.jakub.organistationstructure.infrastructure.output.MongoStructureEntryRepository
+import com.durys.jakub.organistationstructure.infrastructure.output.RabbitmqEventPublisher
+import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.mongodb.core.MongoTemplate
@@ -11,5 +13,9 @@ class OrganisationStructureConfiguration {
 
     @Bean
     fun structureEntryRepository(mongoTemplate: MongoTemplate): StructureEntryRepository = MongoStructureEntryRepository(mongoTemplate)
+
+    @Bean
+    fun eventPublisher(rabbitTemplate: RabbitTemplate): RabbitmqEventPublisher = RabbitmqEventPublisher(rabbitTemplate)
+
 
 }
