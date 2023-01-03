914d51e4fbe0688592479beef65ba2b3645b8e3d
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-03 15:39:45 +0100

    implementation of load in mongo repo

--- src/main/kotlin/com/durys/jakub/organistationstructure/domain/StructureEntryRepository.kt
+++ src/main/kotlin/com/durys/jakub/organistationstructure/domain/StructureEntryRepository.kt
@@ -1,7 +1,7 @@
 package com.durys.jakub.organistationstructure.domain
 
 interface StructureEntryRepository {
-    fun load(id: String): StructureEntry
-    fun loadByPath(path: String): StructureEntry
+    fun load(id: String): StructureEntry?
+    fun loadByPath(path: String): StructureEntry?
     fun save(entry: StructureEntry)
 }
--- src/main/kotlin/com/durys/jakub/organistationstructure/infrastructure/output/MongoStructureEntryRepository.kt
+++ src/main/kotlin/com/durys/jakub/organistationstructure/infrastructure/output/MongoStructureEntryRepository.kt
@@ -3,14 +3,16 @@ package com.durys.jakub.organistationstructure.infrastructure.output
 import com.durys.jakub.organistationstructure.domain.StructureEntry
 import com.durys.jakub.organistationstructure.domain.StructureEntryRepository
 import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.query.Query
+import org.springframework.data.mongodb.core.query.isEqualTo
 
 class MongoStructureEntryRepository(private val mongoTemplate: MongoTemplate): StructureEntryRepository {
 
-    override fun load(id: String): StructureEntry {
-        TODO("Not yet implemented")
+    override fun load(id: String): StructureEntry? {
+        return mongoTemplate.findOne(Query().addCriteria(StructureEntry::id isEqualTo id), StructureEntry::class.java)
     }
 
-    override fun loadByPath(path: String): StructureEntry {
+    override fun loadByPath(path: String): StructureEntry? {
         TODO("Not yet implemented")
     }
 
