f2341ff8e0cd6184e5701ccf2ed8a8a5cc1f2df7
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-05 10:14:34 +0100

    additiona deps in domain Service

--- src/main/kotlin/com/durys/jakub/organistationstructure/domain/OrganisationStructureService.kt
+++ src/main/kotlin/com/durys/jakub/organistationstructure/domain/OrganisationStructureService.kt
@@ -1,4 +1,13 @@
 package com.durys.jakub.organistationstructure.domain
 
-class OrganisationStructureService {
+import com.durys.jakub.organistationstructure.commons.EventPublisher
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class OrganisationStructureService(
+        private val structureEntryRepository: StructureEntryRepository,
+        private val eventPublisher: EventPublisher) {
+
+    
 }
