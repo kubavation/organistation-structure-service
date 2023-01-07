a664c11c75a8b458ea8ed1cd1fd208cf34344cea
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-07 15:08:10 +0100

    tests for checking path in 1 level structure

--- src/test/kotlin/com/durys/jakub/organistationstructure/domain/StructureEntryTest.kt
+++ src/test/kotlin/com/durys/jakub/organistationstructure/domain/StructureEntryTest.kt
@@ -19,5 +19,14 @@ internal class StructureEntryTest {
         assertEquals(entry.path, expectedPath)
     }
 
-  
+    @Test
+    fun createDependantStructureEntry_shouldContainsPathWith1LevelParentShortcut() {
+        val entry = StructureEntry(UUID.randomUUID().toString(), "General Department", "GD")
+        val expectedPath = "GD/DD1"
+
+        entry.addDependant(StructureEntry(UUID.randomUUID().toString(), "Dependant department", "DD1"))
+
+        assertEquals(1, entry.entries.size)
+        assertEquals(entry.entries[0].path, expectedPath)
+    }
 }
