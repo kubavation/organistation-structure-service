3a99359422a8d4310eb0bca4655f1dcdd750b682
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-07 15:01:04 +0100

    path tests #1

---
+++ src/test/kotlin/com/durys/jakub/organistationstructure/domain/StructureEntryTest.kt
@@ -0,0 +1,21 @@
+package com.durys.jakub.organistationstructure.domain
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import java.util.UUID
+
+internal class StructureEntryTest {
+
+
+    @Test
+    fun createStructureEntry_shouldContainsPathWithShortcutOnly() {
+        val entryId = UUID.randomUUID().toString()
+        val name = "General Department"
+        val shortcut = "GD"
+        val path = "GD"
+
+        val entry = StructureEntry(entryId, name, shortcut)
+
+        assertEquals(entry.path, path)
+    }
+}
