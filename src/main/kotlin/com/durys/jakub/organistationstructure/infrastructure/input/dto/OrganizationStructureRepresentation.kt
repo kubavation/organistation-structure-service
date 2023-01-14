83f939c193154a6927f9a93237abdd0e03b4d742
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-14 18:53:04 +0100

    required fields in representation

--- src/main/kotlin/com/durys/jakub/organistationstructure/infrastructure/input/dto/OrganizationStructureRepresentation.kt
+++ src/main/kotlin/com/durys/jakub/organistationstructure/infrastructure/input/dto/OrganizationStructureRepresentation.kt
@@ -2,5 +2,6 @@ package com.durys.jakub.organistationstructure.infrastructure.input.dto
 
 import org.springframework.hateoas.RepresentationModel
 
-class OrganizationStructureRepresentation: RepresentationModel<OrganizationStructureRepresentation>() {
+class OrganizationStructureRepresentation(val id: String, val name: String, val shortcut: String, val path: String)
+    : RepresentationModel<OrganizationStructureRepresentation>() {
 }
