c433337c068207a09f1aeecf6c2f631f9d0972f3
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-14 12:37:47 +0100

    visibility fix

--- src/main/kotlin/com/durys/jakub/organistationstructure/api/assembler/StructureAssembler.kt
+++ src/main/kotlin/com/durys/jakub/organistationstructure/api/assembler/StructureAssembler.kt
@@ -3,7 +3,7 @@ package com.durys.jakub.organistationstructure.api.assembler
 import com.durys.jakub.organistationstructure.api.model.StructureDTO
 import com.durys.jakub.organistationstructure.domain.StructureEntry
 
-object StructureAssembler {
+internal object StructureAssembler {
 
     fun toDTO(entry: StructureEntry) = StructureDTO(entry.id, entry.name, entry.shortcut, entry.path)
 }
