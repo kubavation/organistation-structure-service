bd2109a7a9086b098c5a81dac3ff800874b4f293
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-15 11:32:25 +0100

    removed parentId from createStructureRequest

--- src/main/kotlin/com/durys/jakub/organistationstructure/infrastructure/input/dto/CreateStructureEntryRequest.kt
+++ src/main/kotlin/com/durys/jakub/organistationstructure/infrastructure/input/dto/CreateStructureEntryRequest.kt
@@ -1,6 +1,6 @@
 package com.durys.jakub.organistationstructure.infrastructure.input.dto
 
 
-class CreateStructureEntryRequest(val parentId: String?, val name: String, val shortcut: String) {
-    constructor(): this(null, "","")
+class CreateStructureEntryRequest(val name: String, val shortcut: String) {
+    constructor(): this( "","")
 }
