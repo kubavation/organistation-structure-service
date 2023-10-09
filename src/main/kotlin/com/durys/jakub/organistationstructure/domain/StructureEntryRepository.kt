a8cd6b01ef4a8542afd9865d4538624f3115322a
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-09 19:46:14 +0200

    queries implementation

--- src/main/kotlin/com/durys/jakub/organistationstructure/domain/StructureEntryRepository.kt
+++ src/main/kotlin/com/durys/jakub/organistationstructure/domain/StructureEntryRepository.kt
@@ -5,12 +5,10 @@ import org.springframework.data.jpa.repository.Query
 
 interface StructureEntryRepository: JpaRepository<StructureEntry, String> {
 
-    @Query("from StructureEntry e")
-    fun load(id: String): StructureEntry? = null
-    @Query("from StructureEntry e")
-    fun loadEntryStructureByPath(path: String): StructureEntry? = null
-    @Query("from StructureEntry e")
-    fun save(entry: StructureEntry)
+    @Query("from StructureEntry e where e.id = :id")
+    fun load(id: String): StructureEntry?
+    @Query("from StructureEntry e where e.path = :path")
+    fun loadEntryStructureByPath(path: String): StructureEntry?
     @Query("from StructureEntry e")
     fun loadAll(): List<StructureEntry> = emptyList()
 }
